972da7c0326d050e12b64dfee3919cd436e71b5d
XUQIANLIU <paulliu00426@gmail.com>
2024-12-06 13:12:53 +0800

    countryForm data finished

--- app/src/CountryForm.js
+++ app/src/CountryForm.js
@@ -6,7 +6,14 @@ const CountryForm = () => {
   const [newCountry, setNewCountry] = useState({
     code: '',
     name: '',
-    status: '正常'
+    status: '正常',
+    continent: '',
+    leadershipName: '',
+    affairsName: '',
+    contactName: '',
+    population: '',
+    territorialArea: '',
+    contact: '',
   });
 
   // 獲取國家資料
@@ -29,15 +36,38 @@ const CountryForm = () => {
   };
 
   const handleAddCountry = () => {
-    axios.post('http://172.24.8.156:9998/api/countries', newCountry)
-      .then(response => {
-        setCountryData([...countryData, response.data]);
-        setNewCountry({ code: '', name: '', status: '正常' });
-      })
-      .catch(error => {
-        console.error('There was an error adding the country!', error);
+    const newCountryData = {
+      code: newCountry.code,
+      name: newCountry.name,
+      status: newCountry.status,
+      continent: newCountry.continent,
+      leadershipName: newCountry.leadershipName,
+      affairsName: newCountry.affairsName,
+      contactName: newCountry.contactName,
+      population: newCountry.population,
+      territorialArea: newCountry.territorialArea,
+      contact: newCountry.contact,
+    };
+    axios.post('http://172.24.8.156:9998/api/countries', newCountryData)
+    .then(response => {
+      setCountryData([...countryData, response.data]);
+      setNewCountry({
+        code: '',
+        name: '',
+        status: '正常',
+        continent: '',
+        leadershipName: '',
+        affairsName: '',
+        contactName: '',
+        population: '',
+        territorialArea: '',
+        contact: '',
       });
-  };
+    })
+    .catch(error => {
+      console.error('There was an error adding the country!', error);
+    });
+};
 
   const handleSearchCountry = (code) => {
     return countryData.filter((country) => country.code === code);
@@ -61,6 +91,70 @@ const CountryForm = () => {
           onChange={handleInputChange}
           placeholder="國家名稱"
         />
+        <input
+          type="text"
+          name="continent"
+          value={newCountry.continent}
+          onChange={handleInputChange}
+          placeholder="所屬洲名"
+        />
+
+        <input
+          type="text"
+          name="leadershipName"
+          value={newCountry.leadershipName}  
+          onChange={handleInputChange}  
+          placeholder="元首姓名"  
+        />  
+
+        <input
+          type="text"
+          name="affairsName"
+          value={newCountry.affairsName}  
+          onChange={handleInputChange}  
+          placeholder="外交部長姓名"  
+        />  
+
+        <input
+          type="text"
+          name="contactName"
+          value={newCountry.contactName}  
+          onChange={handleInputChange}  
+          placeholder="聯絡人姓名"  
+        />  
+
+        <input
+          type="text"
+          name="population"
+          value={newCountry.population}  
+          onChange={handleInputChange}  
+          placeholder="人口數"  
+        />  
+
+        <input
+          type="text"
+          name="territorialArea"
+          value={newCountry.area}  
+          onChange={handleInputChange}  
+          placeholder="國土面積"  
+        />
+
+        <input
+          type="text"
+          name="contact"
+          value={newCountry.contact}
+          onChange={handleInputChange}
+          placeholder="聯絡電話"
+        />  
+
+        <input
+          type="text"
+          name="isDiploma"
+          value={newCountry.isDiploma}
+          onChange={handleInputChange}
+          placeholder="是否為邦交國 ( 請輸入是/否 )"
+        />  
+
         <button type="button" onClick={handleAddCountry}>
           新增國家
         </button>
